9d5a50ec4113f702beb5c1c523b8c4be233466dd
piri28 <muhammetpiri28@gmail.com>
2026-05-03 20:17:37 +0300

    Update sitemap.js

--- api/sitemap.js
+++ api/sitemap.js
@@ -1,11 +1,15 @@
 export default function handler(req, res) {
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      <url><loc>https://homeresi.com/</loc></url>
-      <url><loc>https://homeresi.com/mercedes-house</loc></url>
-    </urlset>`;
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://homeresi.com/</loc>
+  </url>
+  <url>
+    <loc>https://homeresi.com/mercedes-house</loc>
+  </url>
+</urlset>`;
 
   res.setHeader('Content-Type', 'text/xml');
-  res.write(sitemap);
+  res.write(sitemap.trim());
   res.end();
 }
